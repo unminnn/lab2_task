b8463a89ec97907472ae67bb0b825a281de64bd1
unmin <unminaru@gmail.com>
2025-02-25 12:31:29 +0200

    added logging for CRUD operations

--- app.js
+++ app.js
@@ -1,11 +1,16 @@
 const bodyParser = require("body-parser");
 const express = require("express");
 const app = express();
+const mymiddlewares = require("./logger")
 const post = require("./post");
 const get = require("./get");
 const put = require("./put");
 const del = require("./delete");
 
+const port = 1234;
+
+
+app.use(mymiddlewares.logger);
 app.use(bodyParser.json());
 app.use(post);
 app.use(get);
@@ -16,9 +21,8 @@ app.get('/', (req,res) => {
     res.send("Response is recived.");
 })
 
-const port = 1234;
-
 app.listen(port, () =>{
     console.log(`Running on port http://localhost:${port}`);
+    console.log(mymiddlewares.logger);
 })
 
---
+++ logger.js
@@ -0,0 +1,11 @@
+const express = require("express");
+
+const logger = (req, res, next) => {
+    console.log(`[${new Date().toISOString()}] ${req.method} ${req.url}`);
+
+    next();
+}
+
+module.exports = {
+    logger
+}
